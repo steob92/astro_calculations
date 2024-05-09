b674c8be79348b090a1b4c046974aa2f16ce3200
Ste O'Brien <stephan.obrien@mcgill.ca>
2024-05-09 14:44:12 -0400

    Adding a range test

--- test.py
+++ test.py
@@ -7,3 +7,9 @@ assert(add(x, y) == 3)
 assert(sub(x, y) == -1)
 assert(mult(x, y) == 2)
 assert(div(x, y) == 0.5)
+
+for i  in range(1, 10):
+    assert( add(i,i) == i + i )
+    assert( sub(i,i) == i - i )
+    assert( mult(i,i) == i * i )
+    assert( div(i,i) == i / i )
