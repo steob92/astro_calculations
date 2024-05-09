7dde610ef1abc2be3443ae84d44d841421ddd219
Ste O'Brien <stephan.obrien@mcgill.ca>
2024-05-09 14:39:39 -0400

    Adding more advanced tests using numpy

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from astro_calculations import add, sub, mult, div
+import numpy as np
 
 x = 1
 y = 2
@@ -7,3 +8,14 @@ assert(add(x, y) == 3)
 assert(sub(x, y) == -1)
 assert(mult(x, y) == 2)
 assert(div(x, y) == 0.5)
+
+
+x = np.linspace(1, 10)
+y = np.linspace(10, 20)
+
+
+
+assert(np.all(add(x, y) == x + y))
+assert(np.all(sub(x, y) == x - y))
+assert(np.all(mult(x, y) == x * y))
+assert(np.all(div(x, y) == x / y))
