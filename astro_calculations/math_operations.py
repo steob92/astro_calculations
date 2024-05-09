4fe0b5fbdab1a166ec37938d0e6ba346fc0e7b79
Ste O'Brien <stephan.obrien@mcgill.ca>
2024-05-09 14:43:16 -0400

    adding debug option

--- astro_calculations/math_operations.py
+++ astro_calculations/math_operations.py
@@ -1,6 +1,6 @@
 from .printing import print_details
 
-def add(a, b):
+def add(a, b, debug = False):
     """Add two numbers together
 
     Args:
@@ -10,10 +10,11 @@ def add(a, b):
     Returns:
         a + b
     """
-    print_details(a,b)
+    if debug:
+        print_details(a,b)
     return a + b
 
-def sub(a, b):
+def sub(a, b, debug = False):
     """Subtract two numbers together
 
     Args:
@@ -23,10 +24,11 @@ def sub(a, b):
     Returns:
         a - b
     """
-    print_details(a,b)
+    if debug:
+        print_details(a,b)
     return a - b
 
-def mult(a, b):
+def mult(a, b, debug = False):
     """Multiply two numbers together
 
     Args:
@@ -36,10 +38,11 @@ def mult(a, b):
     Returns:
         a * b
     """
-    print_details(a,b)
+    if debug:
+        print_details(a,b)
     return a * b
 
-def div(a, b):
+def div(a, b, debug = False):
     """Divide one number by another
 
     Args:
@@ -49,5 +52,6 @@ def div(a, b):
     Returns:
         a / b
     """
-    print_details(a,b)
+    if debug:
+        print_details(a,b)
     return a / b
